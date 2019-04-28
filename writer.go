fc25812ee1cc929a0b62e930e22c1fe2388c64bc
Ryan J Yoder <me@ryanjyoder.com>
2019-04-27 20:59:00 -0700

    more directory per subdirs

--- writer.go
+++ writer.go
@@ -26,11 +26,9 @@ func (w *streamWriter) Write(d Delta) error {
 		streamID = "0000nostream"
 	}
 	paddedStream := d.StreamID() + "00000000"
-	dir1 := paddedStream[:2]
-	dir2 := paddedStream[2:4]
-	dir3 := paddedStream[4:6]
-	dir4 := paddedStream[6:8]
-	fullDirPath := filepath.Join(w.baseDir, dir1, dir2, dir3, dir4)
+	dir1 := paddedStream[:3]
+	dir2 := paddedStream[3:8]
+	fullDirPath := filepath.Join(w.baseDir, dir1, dir2)
 	fullFilePath := filepath.Join(fullDirPath, streamID)
 	os.MkdirAll(fullDirPath, 0755)
 	f, err := os.OpenFile(fullFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
