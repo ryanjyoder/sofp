df319bed30b28a9133583451f9c9d825ec7185aa
Ryan J Yoder <me@ryanjyoder.com>
2019-06-09 14:13:07 -0700

    back to basics (add fd pool)

--- parse.go
+++ parse.go
@@ -3,36 +3,73 @@ package sofp
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
-
-	"github.com/ryanjyoder/couchdb"
 )
 
 func (w *Worker) parseDomain(domain string) error {
 	log.Println("Starting to parse:", domain)
+	isExported, err := w.domainIsExported(domain)
+	if isExported || err != nil {
+		return err // err will be nil if already exported
+	}
 
-	lookup, err := w.getStreamLookup(domain)
+	deltaChan, err := w.getDeltaChan(domain)
 	if err != nil {
 		return err
 	}
 
-	db, err := w.prepareDB(domain)
+	partialDir := filepath.Join(w.workingDir, "streams", domain+"partial")
+	completedDir := filepath.Join(w.workingDir, "streams", domain)
+	os.MkdirAll(partialDir, 0755)
+	if err := os.RemoveAll(partialDir); err != nil {
+		return err
+	}
+	if err := os.MkdirAll(partialDir, 0755); err != nil {
+		return err
+	}
+	err = writeDeltas(partialDir, deltaChan)
 	if err != nil {
 		return err
 	}
 
-	err = w.parseToCouch(domain, lookup, db)
-	if err != nil {
-		return err
-	}
-
-	err = w.exportCouchStreams(db, domain)
+	err = os.Rename(partialDir, completedDir)
 
 	return err
+}
+
+func writeDeltas(exportDir string, deltaChan chan *Row) error {
+	fdpool, err := NewFDPool(100)
+	if err != nil {
+		return err
+	}
+	defer fdpool.CloseAll()
+
+	for delta := range deltaChan {
+		subDir := ("000" + delta.StreamID)[len(delta.StreamID):]
+		outputDir := filepath.Join(exportDir, subDir)
+		err := os.MkdirAll(outputDir, 0755)
+		if err != nil {
+			return err
+		}
+		outputFilename := filepath.Join(outputDir, delta.StreamID)
+		output, err := fdpool.GetFD(outputFilename)
+		if err != nil {
+			return err
+		}
+		jsonBytes, err := json.Marshal(delta)
+		if err != nil {
+			return err
+		}
+		_, err = output.Write(append(jsonBytes, '\n'))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (w *Worker) domainIsExported(domain string) (bool, error) {
@@ -50,162 +87,52 @@ func (w *Worker) domainIsExported(domain string) (bool, error) {
 	return false, err
 }
 
-func (w *Worker) exportCouchStreams(db couchdb.DatabaseService, domain string) error {
-	log.Println("exporting from couchdb:", domain)
-	isExport, err := w.domainIsExported(domain)
+func (w *Worker) getDeltaChan(domain string) (chan *Row, error) {
+
+	postsPsr, err := w.getXmlReader(domain, PostsType)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	if isExport {
-		return nil
-	}
-	exportDir := filepath.Join(w.workingDir, "streams", domain+".partial")
-	os.MkdirAll(exportDir, 0755)
-	err = os.RemoveAll(exportDir)
+	historyPsr, err := w.getXmlReader(domain, PostHistoryType)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	//exportDir := filepath.Join(w.workingDir, "streams", domain+".partial")
-	itr, err := NewQueryIterator(db, "streams", "byStreamID")
+	commentsPsr, err := w.getXmlReader(domain, CommentsType)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	var streamID, deltaType string
-	var output io.WriteCloser
-	defer func() {
-		if output != nil {
-			output.Close()
+	deltaChan := make(chan *Row)
+	go func() {
+		defer close(deltaChan)
+		lookup := make([]uint32, 100*1000*1000)
+		for post := postsPsr.Next(); post != nil; post = postsPsr.Next() {
+			if post.PostTypeID == "2" {
+				lookup[*post.ID] = uint32(*post.ParentID)
+			} else {
+				lookup[*post.ID] = uint32(*post.ID)
+			}
+			for historyPsr.Peek() != nil && *historyPsr.Peek().PostID <= *post.ID {
+				d := historyPsr.Next()
+				d.DeltaType = PostHistoryType
+				d.StreamID = fmt.Sprint(lookup[*d.PostID])
+				deltaChan <- d
+			}
+			for commentsPsr.Peek() != nil && *commentsPsr.Peek().PostID <= *post.ID {
+				d := commentsPsr.Next()
+				d.DeltaType = CommentsType
+				d.StreamID = fmt.Sprint(lookup[*d.PostID])
+				deltaChan <- d
+			}
 		}
 	}()
-	row, err := itr.Next()
-	if err != nil {
-		return err
-	}
-	for row != nil {
-		nextStreamID, nextDeltaType, err := extractKeys(row.Key)
-		if err != nil {
-			return err
-		}
-		if nextStreamID != streamID || nextDeltaType != deltaType {
-			if output != nil {
-				output.Close()
-			}
-			streamID, deltaType = nextStreamID, nextDeltaType
-			subDir := string("000" + streamID)[len(streamID):]
-			outputDir := filepath.Join(exportDir, subDir, streamID)
-			err = os.MkdirAll(outputDir, 0755)
-			if err != nil {
-				return err
-			}
-			outputFilename := filepath.Join(outputDir, deltaType)
-			output, err = os.Create(outputFilename)
-			if err != nil {
-				return err
-			}
-		}
-		jsonBytes, err := json.Marshal(row.Value)
-		if err != nil {
-			return err
-		}
-		_, err = output.Write(append(jsonBytes, '\n'))
-		if err != nil {
-			return err
-		}
 
-		row, err = itr.Next()
-		if err != nil {
-			return fmt.Errorf("Error reading rows:", err)
-		}
-	}
-
-	completedDir := filepath.Join(w.workingDir, "streams", domain)
-	return os.Rename(exportDir, completedDir)
-}
-
-func (w *Worker) parseToCouch(domain string, lookup []int, db couchdb.DatabaseService) error {
-
-	historyReader, err := w.getXmlReader(domain, PostHistoryType)
-	if err != nil {
-		return err
-	}
-	defer historyReader.Close()
-	psr, err := NewParser(historyReader)
-	if err != nil {
-		return err
-	}
-	lastSeenID, err := getLastSeenID(db, PostHistoryType)
-	if err != nil {
-		return err
-	}
-	if lastSeenID != 0 {
-		log.Println("resetting parser to checkpoint:", lastSeenID)
-	}
-	n := 25
-	bulk := []couchdb.CouchDoc{}
-	for row := psr.Next(); row != nil; row = psr.Next() {
-		if lastSeenID >= *row.ID {
-			if lastSeenID == *row.ID {
-				log.Println("ok reset. begining insert")
-			}
-			continue
-		}
-		streamID := lookup[*row.PostID]
-		row.Stream = fmt.Sprintf("%d", streamID)
-		row.DeltaType = PostHistoryType
-		row.DeltaID = row.GetID()
-		bulk = append(bulk, row)
-		if len(bulk) >= n {
-			_, err := db.Bulk(bulk)
-			if err != nil {
-				cErr, ok := err.(*couchdb.Error)
-				if !ok || cErr.StatusCode != 409 {
-					return err
-				}
-			}
-			bulk = []couchdb.CouchDoc{}
-		}
-	}
-	if len(bulk) > 0 {
-		_, err := db.Bulk(bulk)
-		return err
-	}
-	return nil
+	return deltaChan, nil
 
 }
 
-func (w *Worker) getStreamLookup(domain string) ([]int, error) {
-	log.Println("Preparing lookup for:", domain)
-
-	stdout, err := w.getXmlReader(domain, "Posts")
-	if err != nil {
-		return nil, err
-	}
-	defer stdout.Close()
-	psr, err := NewParser(stdout)
-	if err != nil {
-		return nil, err
-	}
-
-	lookup := make([]int, 75*1000*1000)
-
-	for row := psr.Next(); row != nil; row = psr.Next() {
-		if row.err != nil {
-			fmt.Println("error parsing row:", row.err)
-			continue
-		}
-		if row.PostTypeID == "1" {
-			lookup[*row.ID] = *row.ID
-		}
-		if row.PostTypeID == "2" {
-			lookup[*row.ID] = *row.ParentID
-		}
-	}
-	return lookup, nil
-}
-
-func (w *Worker) getXmlReader(domain string, deltaType string) (io.ReadCloser, error) {
+func (w *Worker) getXmlReader(domain string, deltaType string) (*RowsParser, error) {
 	postZipFilanme := filepath.Join(w.workingDir, "zips", domain+".7z")
 	_, err := os.Stat(postZipFilanme)
 	// hmmm try with '-Post' postfix
@@ -222,40 +149,10 @@ func (w *Worker) getXmlReader(domain string, deltaType string) (io.ReadCloser, e
 	go func() {
 		cmd.Wait()
 	}()
-	return stdout, err
+
+	psr, err := NewParser(stdout)
+	if err != nil {
+		return nil, err
+	}
+	return psr, err
 }
-
-/*
-
-func (w *Worker) getKeyValue(key []byte) ([]byte, error) {
-	var val []byte
-	err := w.badger.View(func(txn *badger.Txn) error {
-		item, err := txn.Get(key)
-		if err != nil {
-			return err
-		}
-
-		val, err = item.Value()
-		return err
-	})
-
-	return val, err
-}
-func (w *Worker) setKeyValue(key, value []byte) error {
-	return w.badger.Update(func(txn *badger.Txn) error {
-		err := txn.Set(key, value)
-		return err
-	})
-}
-
-func intToBytes(n int) []byte {
-	buf := make([]byte, binary.MaxVarintLen64)
-	binary.PutVarint(buf, int64(n))
-	return buf
-}
-
-func bytesToInt(b []byte) int {
-	x, _ := binary.Varint(b)
-	return int(x)
-}
-*/
--- worker.go
+++ worker.go
@@ -4,14 +4,12 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/url"
 	"os"
 	"path/filepath"
 	"strconv"
 	"sync"
 	"time"
 
-	"github.com/ryanjyoder/couchdb"
 	"golang.org/x/sync/semaphore"
 )
 
@@ -19,17 +17,12 @@ type Worker struct {
 	workingDir        string
 	downloadSemephore *semaphore.Weighted
 	parseSemephore    *semaphore.Weighted
-	couchClient       couchdb.ClientService
-	//badger            *badger.DB
 }
 
 type WorkerConfigs struct {
 	StorageDirectory      string
 	SimultaneousDownloads int64
 	SimultaneousParsers   int64
-	CouchDBHost           string
-	CouchDBUser           string
-	CouchDBPass           string
 }
 
 func NewWorker(configs WorkerConfigs) (*Worker, error) {
@@ -38,37 +31,10 @@ func NewWorker(configs WorkerConfigs) (*Worker, error) {
 		return nil, err
 	}
 
-	u, err := url.Parse(configs.CouchDBHost)
-	if err != nil {
-		return nil, err
-	}
-	// create a new client
-	client, err := couchdb.NewAuthClient(configs.CouchDBUser, configs.CouchDBPass, u)
-	if err != nil {
-		return nil, err
-	}
-
-	/*
-		// Open the Badger database located in the /tmp/badger directory.
-		// It will be created if it doesn't exist.
-		opts := badger.DefaultOptions
-		opts.Dir = filepath.Join(workingDir, "badger")
-		opts.ValueDir = opts.Dir
-		opts.MaxTableSize = 1 << 20
-		opts.NumMemtables = 1
-		opts.NumCompactors = 1
-		opts.TableLoadingMode = options.MemoryMap
-
-		db, err := badger.Open(opts)
-		if err != nil {
-			return nil, err
-		}
-	*/
 	return &Worker{
 		workingDir:        workingDir,
 		downloadSemephore: semaphore.NewWeighted(configs.SimultaneousDownloads),
 		parseSemephore:    semaphore.NewWeighted(configs.SimultaneousParsers),
-		couchClient:       client,
 	}, nil
 }
 
@@ -113,23 +79,6 @@ func GetDefaultConfigs() (WorkerConfigs, error) {
 	if len(os.Args) < 2 {
 		return cfg, fmt.Errorf("storage directory not provided")
 	}
-	couchdbHost, ok := os.LookupEnv("COUCHDB_HOST")
-	if !ok {
-		return cfg, fmt.Errorf("env COUCHDB_HOST not set")
-	}
-	cfg.CouchDBHost = couchdbHost
-
-	couchdbUser, ok := os.LookupEnv("COUCHDB_USER")
-	if !ok {
-		return cfg, fmt.Errorf("env COUCHDB_USER not set")
-	}
-	cfg.CouchDBUser = couchdbUser
-
-	couchdbPass, ok := os.LookupEnv("COUCHDB_PASS")
-	if !ok {
-		return cfg, fmt.Errorf("env COUCHDB_HOST not set")
-	}
-	cfg.CouchDBPass = couchdbPass
 
 	simDownloadStr, _ := os.LookupEnv("SIMULTANEOUS_DOWNLOAD")
 	if simDownloadStr == "" {
